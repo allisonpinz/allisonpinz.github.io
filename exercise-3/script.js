cee26652067afd7231a93d21173ac13a0e3a961d
AllisonP910 <allison.pinz910@gmail.com>
2017-02-23 20:33:41 -0500

    Exercise3

---
+++ exercise-3/script.js
@@ -0,0 +1,73 @@
+$(document).ready(function() {
+	$('#part1').click(function() {
+		$('#part1').fadeOut('slow');
+	});
+});
+
+$(document).ready(function(){ 
+
+ $("#part1").click(function() { 
+     $("#part1").fadeIn("slow");
+ });
+
+ $("#part1").click(function() { 
+  $("#part1").fadeOut("slow"); 
+  });
+
+  $("#part2").click(function() { 
+     $("#part2").fadeIn("slow");
+ });
+
+ $("#part2").click(function() { 
+  $("#part2").fadeOut("slow"); 
+  });
+
+  $("#part3").click(function() { 
+     $("#part3").fadeIn("slow");
+ });
+
+ $("#part3").click(function() { 
+  $("#part3").fadeOut("slow"); 
+  });
+
+  $("#part4").click(function() { 
+     $("#part4").fadeIn("slow");
+ });
+
+ $("#part5").click(function() { 
+  $("#part5").fadeOut("slow"); 
+  });
+
+  $("#part5").click(function() { 
+  $("#part5").fadeOut("slow"); 
+  });
+
+  $("#part6").click(function() { 
+     $("#part6").fadeIn("slow");
+ });
+
+ $("#part6").click(function() { 
+  $("#part6").fadeOut("slow"); 
+  });
+
+  $("#part7").click(function() { 
+     $("#part7").fadeIn("slow");
+ });
+
+ $("#part8").click(function() { 
+  $("#part8").fadeOut("slow"); 
+  });
+
+  $("#part8").click(function() { 
+  $("#part8").fadeOut("slow"); 
+  });
+
+  $("#part9").click(function() { 
+     $("#part9").fadeIn("slow");
+ });
+
+ $("#part9").click(function() { 
+  $("#part9").fadeOut("slow"); 
+  });
+
+ });
